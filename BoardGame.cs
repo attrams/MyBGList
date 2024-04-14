e212f48b3e1b5db2483666f63b228820ddbe3cc8
attrams <48452451+Attrams@users.noreply.github.com>
2024-04-14 08:32:32 +0000

    update BoardGame.cs model

--- BoardGame.cs
+++
@@ -1,9 +0,0 @@
-namespace MyBGList
-{
-    public class BoardGame
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public int? Year { get; set; }
-    }
-}
