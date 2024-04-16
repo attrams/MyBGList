47b504bf9feb646c178001b3babb2b71f5f7d4e7
attrams <48452451+Attrams@users.noreply.github.com>
2024-04-16 06:53:10 +0000

    update the Get()

--- Controllers/BoardGamesController.cs
+++ Controllers/BoardGamesController.cs
@@ -1,3 +1,4 @@
+using System.Linq.Dynamic.Core;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyBGList.DTO;
@@ -20,15 +21,25 @@ namespace MyBGList.Controllers
 
         [HttpGet(Name = "GetBoardGames")]
         [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Any)]
-        public async Task<RestDTO<BoardGame[]>> Get()
+        public async Task<RestDTO<BoardGame[]>> Get(int pageIndex = 0, int pageSize = 10, string? sortColumn = "Name", string? sortOrder = "ASC", string? filterQuery = null)
         {
-            var query = _context.BoardGames;
+            var query = _context.BoardGames.AsQueryable();
+
+            if (!string.IsNullOrEmpty(filterQuery))
+            {
+                query = query.Where(b => b.Name.Contains(filterQuery));
+            }
+            var recordCount = await query.CountAsync();
+            query = query.OrderBy($"{sortColumn} {sortOrder}").Skip(pageIndex * pageSize).Take(pageSize);
 
             return new RestDTO<BoardGame[]>()
             {
                 Data = await query.ToArrayAsync(),
+                PageIndex = pageIndex,
+                PageSize = pageSize,
+                RecordCount = recordCount,
                 Links = new List<LinkDTO>{
-                    new LinkDTO(Url.Action(null, "BoardGames", null, Request.Scheme)!, "self", "GET")
+                    new LinkDTO(Url.Action(null, "BoardGames", new {pageIndex, pageSize}, Request.Scheme)!, "self", "GET")
                 }
             };
         }
